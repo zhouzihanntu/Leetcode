ae3ee4dc4a13a211ecb94caa15fd5da7351068fa
肘子涵 <abbey357423606@hotmail.com>
2020-04-21 00:12:46 +0800

    200-java

--- 200. Number of Islands/numIslands.java
+++ 200. Number of Islands/numIslands.java
@@ -1,36 +1,32 @@
-class Solution {
-    public int numIslands(char[][] grid) {
+class numIslands {
+    public int solution(char[][] grid) {
         if (grid == null || grid.length == 0) {
             return 0;
         }
-
+        int cnt = 0;
         int width = grid.length;
         int length = grid[0].length;
-        int cnt = 0;
-        for (int r = 0; r < width; ++r) {
-            for (int c = 0; c < length; ++c) {
-                if (grid[r][c] == '1') {
-                    ++cnt;
-                    dfs(grid, r, c);
+        for (int i = 0; i < width; i ++) {
+            for (int j = 0; j < length; j ++) {
+                if (grid[i][j] == '1') {
+                    cnt ++;
+                    dfs(grid, i, j);
                 }
             }
         }
-
         return cnt;
     }
 
-    public void dfs(char[][] grid, int r, int c) {
+    public void dfs(char[][] grid, int x, int y) {
         int width = grid.length;
         int length = grid[0].length;
-
-        if (r < 0 || c < 0 || r >= width || c >= length || grid[r][c] == '0') {
+        if (x < 0 || x >= width || y < 0 || y >= length || grid[x][y] == '0') {
             return;
         }
-
-        grid[r][c] = '0';
-        dfs(grid, r - 1, c);
-        dfs(grid, r + 1, c);
-        dfs(grid, r, c - 1);
-        dfs(grid, r, c + 1);
+        grid[x][y] = '0';
+        dfs(grid, x - 1, y);
+        dfs(grid, x + 1, y);
+        dfs(grid, x, y - 1);
+        dfs(grid, x, y + 1);
     }
 }
